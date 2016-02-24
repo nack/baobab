b8f165d329d2557253bacd56eb8555b17fdf03c8
nack <nack555@gmail.com>
2016-02-24 18:00:00 +0900

    Googleから画像取得(非整形)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,26 +3,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  require 'cgi'
-  require 'pp'
+
 
   def index
-    #
-    # search_word_org = 'バオバブ'
-    # limit = 5
-    #
-    # search_word = CGI.escape(search_word_org)
-    # url = URI("#{configatron.google_api_url}?v=1.0&key=#{configatron.google_api_key}&q=#{search_word}&rsz=#{limit}")
-    # http = Net::HTTP.new(url.host)
-    # response = http.get(url.request_uri)
-    # response_data = JSON.parse(response.body)
-    # if response_data['responseStatus'] == 403
-    #   return image_list = nil
-    # end
-    # image_list = response_data['responseData']['results']
-    #
-    # pp image_list.inspect
+    require 'open-uri'
+    url = "https://www.google.co.jp/search?q=%E3%83%88%E3%82%A4%E3%83%AC%E3%83%83%E3%83%88%E3%83%9B%E3%83%AB%E3%83%80%E3%83%BC&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjZyePXg5DLAhXBnJQKHQtrDdUQ_AUIBygB&biw=1339&bih=860"
+    #URL開く
+    html = open(url).read
+    #Nokogiriでパース
+    parsed_html = Nokogiri::HTML(html)
+    #imgタグだけ取得
+    images = parsed_html.css('img')
 
+    #raise images.inspect
+    @imgsrcs = []
+    if images.present?
+        # Check the size with RMagick
+        proper_image = ""
+        images.each do |image|
+            @imgsrcs.push image.attributes["src"].value
+        end
+    end
+    @imgsrcs.uniq!
 
   end
 
